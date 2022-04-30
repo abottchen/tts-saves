5095efc2a592d9f64d9402fd7dc6c2f64ff87c20
Adam Bottchen <adam.bottchen@puppet.com>
2022-04-29 23:47:50 -0700

    readme update and update script

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ echo "# tts-saves" > README.md
 
 for file in $(ls *.png); do
   echo $(basename $file .png) >> README.md
+  echo "<br>" >> README.md
   echo "![$file]($file)" >> README.md
   echo "<br>" >> README.md
 done
